11e992fa675783910c5bdb46f25069a47ad73b34
arijitswain23 <arijitswain369@gmail.com>
2024-03-09 02:12:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 # Create your views here.
 from app.forms import *
 
-
+@login_required
 def homee(request):
     return render(request,'play.html')
 
@@ -56,14 +56,14 @@ def login_form(request):
 @login_required
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('home'))
+    return HttpResponseRedirect(reverse('login_form'))
 
 
-
+@login_required
 def home(request):
     return render(request,'movies.html')
 
-
+@login_required
 def kid(request):
     return render(request,'kids.html')
 
@@ -81,20 +81,20 @@ def aha(request):
     return render(request,'aha.html')
 
 
-
+@login_required
 def indian_idel(request):
     return render(request,'indian_idel.html')
 
-
+@login_required
 def shows(request):
     return render(request,'shows.html')
 
 
-
+@login_required
 def My_aha(request):
     return render(request,'My_aha.html')
 
-
+@login_required
 def subscribe(request):
     return render(request,'subscribe.html')
 
@@ -130,12 +130,42 @@ def reset_password(request):
     return render(request,'reset_password.html')
 
 #Uploaded Videos
+@login_required
 def hanuman(request):
     return render(request,'hanuman.html')
-
+@login_required
 def ind1(request):
     return render(request,'ind1.html')
-
+@login_required
 def ind2(request):
     return render(request,'ind2.html')
+
+@login_required
+def movie1(request):
+    return render(request,'movie1.html')
+
+@login_required
+def movie2(request):
+    return render(request,'movie2.html')
+
+@login_required
+def movie3(request):
+    return render(request,'movie3.html')
+
+@login_required
+def movie4(request):
+    return render(request,'movie4.html')
+
+@login_required
+def movie5(request):
+    return render(request,'movie5.html')
+
+@login_required
+def krishna(request):
+    return render(request,'krishna.html')
+
+@login_required
+def ganesh(request):
+    return render(request,'ganesh.html')
+
 
