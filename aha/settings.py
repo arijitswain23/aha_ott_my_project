c35aa4756169d3104a3d2bae53ca612cada3a25f
Arijit Swain <150240882+arijitswain23@users.noreply.github.com>
2025-01-28 16:29:25 +0530

    Update settings.py

--- aha/settings.py
+++ aha/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = "static/"
+STATIC_URL = "static/"
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/ec2-user/aha_ott_my_project/static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = '/home/ec2-user/aha_ott_my_project/static/'
 
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
